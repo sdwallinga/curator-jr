94d9d09dfc2a48fe99f5893a21b5b0c6530b8bf2
Sam Wallinga <sam@5thcolumn.net>
2017-11-09 16:18:37 -0600

    Added storage aggregation

--- get-doc-counts.py
+++ get-doc-counts.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 from datetime import datetime, timedelta
 from elasticsearch import Elasticsearch
-import subprocess
 
 # Tenant's info goes here
-name_prefix = '5thc'
-haproxy_ip = '172.27.101.27'
+name_prefix = 'ies'
+haproxy_ip = '63.141.41.169'
 sla_days = 30
 
 es = Elasticsearch([haproxy_ip], port=9200)
@@ -16,6 +15,7 @@ cutoff = cutoff.strftime('%Y.%m.%d')
 indices = sorted(es.indices.get_alias("*"))
 toArchive=[]
 toSum=[]
+sizeSum=0
 for i in indices:
   i = i.split('-')
   if (len(i) == 2):
@@ -24,7 +24,16 @@ for i in indices:
       toArchive.append(index)
 
 for i in toArchive:
-    count = es.cat.count(index=name_prefix + "-" + i, filter_path=['count'])
+    target = name_prefix + "-" + i
+    count = es.cat.count(index=target, filter_path=['count'])
+    size = es.indices.stats(index=target, metric=['store'])
     toSum.append(int(count[0]['count']))
+    sizeSum += int(size['indices'][target]['total']['store']['size_in_bytes'])
 
-print(sum(toSum))
+records = str(sum(toSum))
+size = str(sizeSum)
+
+print(f'For the last {sla_days} days:')
+print('---------------------')
+print(f'Total records: {records}')
+print(f'Total size in bytes: {size}b')
